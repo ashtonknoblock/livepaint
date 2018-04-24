dadc6c6cf8cdaa2c42dfd3a23fc05d6411256de6
Jake Berg <jacobvictorberg@gmail.com>
2018-04-24 18:09:56 -0400

    now it works based on last commit

--- public/polling.js
+++ public/polling.js
@@ -16,12 +16,10 @@ function fetchUpdates() {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            clientUpdates,
-            clientCounter
+            clientUpdates
         })
     }
 
-    let clientCounter = 0;
     fetch("/updates", postOptions)
         .then(response => response.json())
         .then(data => {
@@ -30,20 +28,14 @@ function fetchUpdates() {
             let newRow = 0;
             let newCol = 0;
             let newColor = "";
-            let goodCommands = data.updates.slice(clientCounter)
             for(let i = 0; i < data.updates.length; i++){
                 newRow = data.updates[i][0];
                 newCol = data.updates[i][1];
                 newColor = data.updates[i][2];
-                clientCounter++
                 // console.log("row:", newRow, "col", newCol, "color", newColor);
-               
                 bitmap.setColor(newRow, newCol, newColor, runCommand);
                 bitmap.fill(newRow, newCol, newColor)
             }
-            
-            console.log(clientCounter)
-            console.log(goodCommands)
             clientUpdates = data.reset
         }).catch(function (error) {
             errors++
